6ccf61f5c98eebd2004f6874184525759f98fc11
Deep Patel <dp8@email.sc.edu>
2022-08-28 20:59:19 -0400

    WIP

--- strategy/CrawlBehavior.java
+++ strategy/CrawlBehavior.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+
 public class CrawlBehavior {
-    
+    public void move(ArrayList<String> character, int speed) {
+        speed = 1;
+        for(Robot HumanRobot: robots)
+        {
+            System.out.println(HumanRobot);
+        }
+        for(Robot iRobot: robots)
+        {
+            System.out.println(iRobot);
+        }
+        for(Robot DogRobot: robots)
+        {
+            System.out.println(DogRobot);
+        }    
+    }
 }
--- strategy/WalkBehavior.java
+++ strategy/WalkBehavior.java
@@ -1,3 +1,17 @@
 public class WalkBehavior {
-    
+    public void move(ArrayList<String> character, int speed) {
+        speed = 2;
+        for(Robot HumanRobot: robots)
+        {
+            System.out.println(HumanRobot);
+        }
+        for(Robot iRobot: robots)
+        {
+            System.out.println(iRobot);
+        }
+        for(Robot DogRobot: robots)
+        {
+            System.out.println(DogRobot);
+        } 
+    }
 }
