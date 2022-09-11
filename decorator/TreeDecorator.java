8b71ac4bdc79c96d79bf2725e934abcb5016a3d8
Deep Patel <dp8@email.sc.edu>
2022-09-11 19:38:27 -0400

    FINISHED

--- decorator/TreeDecorator.java
+++ decorator/TreeDecorator.java
@@ -20,9 +20,15 @@ abstract class TreeDecorator extends Tree{
          for(int i=0;i<decor.size();i++) {
             String temp = "";
             for(int j=0;j<decor.get(i).length();j++) {
+                /**
+                 * If character found in decor is not empty, set character in string to character in decor
+                 */
                 if(decor.get(i).charAt(j) != ' ') {
                     temp += decor.get(i).charAt(j);
                 }
+                /**
+                 * Set character to character found in the line array if char in decor array is not found
+                 */
                 else {
                     temp += lines.get(i).charAt(j);
                 }
