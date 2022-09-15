4044855b892494586f57dceb2c416ac8735437e3
Deep Patel <dp8@email.sc.edu>
2022-09-15 11:30:27 -0400

    ?

---
+++ factory/Cereal.java
@@ -0,0 +1,9 @@
+package factory;
+
+import java.util.ArrayList;
+
+abstract class Cereal {
+    protected String name;
+    protected double price;
+    protected ArrayList<String> toys;
+}
---
+++ factory/LuckyCharms.java
@@ -0,0 +1,5 @@
+package factory;
+
+public class LuckyCharms implements Cereal{
+    
+}
