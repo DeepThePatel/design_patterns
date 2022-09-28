fdcfa4d37da6ebd5991f5ddf50556d0407217c1e
Deep Patel <dp8@email.sc.edu>
2022-09-28 14:59:27 -0400

    initial commit

---
+++ singleton/ClothingItem.java
@@ -0,0 +1,22 @@
+public class ClothingItem {
+    private String name;
+    private ClothingPart part;
+    private String color;
+    private ArrayList<Season> seasons;
+
+    public ClothingItem(String name, ClothingPart part, String color) {
+
+    }
+
+    public boolean hasSeason(Season season) {
+
+    }
+
+    public void addSeason(Season season) {
+
+    }
+
+    public String toString(){
+        
+    }
+}
---
+++ singleton/OutfitCreator.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.random;
+
+public class OutfitCreator {
+    private ArrayList<ClothingItem> tops;
+    private ArrayList<ClothingItem> bottoms;
+    private ArrayList<ClothingItem> wholes;
+    Random rand;
+    private static OutfitCreator outfitCreator;
+
+    private OutfitCreator() {
+
+    }
+
+    public static OutfitCreator getInstance() {
+
+    }
+
+    public String getOutfit(Season season) {
+
+    }
+}
